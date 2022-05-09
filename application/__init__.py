cc69769d0af24011555f66667f001e975f422402
waynemorphic <wayneklyde@gmail.com>
2022-05-09 19:21:51 +0300

    initialization

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,17 @@ from .config import DevConfig
 from flask_bootstrap import Bootstrap
 from .config import config_options
 from flask_sqlalchemy import SQLAlchemy 
+from flask_login import LoginManager
+# from . import views, forms
 
 # extensions initialization
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+# login manager
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'main.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,7 +24,8 @@ def create_app(config_name):
     # flask extension initialization
     bootstrap.init_app(app)
     db.init_app(app)
-    
+    login_manager.init_app(app)
+
     # registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -26,5 +34,5 @@ def create_app(config_name):
 
 
 
-# from application.main import views
+
 # from application.main import error
--- application/auth/__init__.py
+++ application/auth/__init__.py
@@ -1,12 +1,5 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,PasswordField,SubmitField, BooleanField # |boolean renders checkbox in our form
-# from wtforms.validators import InputRequired,Email,EqualTo
-# # from ..models import User
-# from wtforms import ValidationError
+from flask import Blueprint
 
-# # login a user with existing credentials
-# class LoginForm(FlaskForm):
-#     email = StringField('Your Email Address',validators=[InputRequired(),Email()])
-#     password = PasswordField('Password',validators =[InputRequired()])
-#     remember = BooleanField('Remember me') # boolean to confirm wheter the user wants to be logged out after the session.
-#     submit = SubmitField('Sign In')
+auth = Blueprint('auth',__name__)
+
+# from . import views
