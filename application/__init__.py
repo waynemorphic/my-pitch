890e97d4d55d9f06b4fcb6f5a24fae7427cf81fa
waynemorphic <wayneklyde@gmail.com>
2022-05-09 12:55:50 +0300

    change of file and flder structure

--- application/__init__.py
+++ application/__init__.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from .config import config_options
 
-# application initialization
-app = Flask(__name__, instance_relative_config = True)
 
-# configurations setup
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+bootstrap = Bootstrap()
 
-# flask extension initialization
-bootstrap = Bootstrap(app)
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    # creating app configurations
+    app.config.from_object(config_options[config_name])
+    
+    # flask extension initialization
+    bootstrap.init_app(app)
+    
+    # registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app
 
-from application import views
+
+
+# from application.main import views
+# from application.main import error
--- run.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap
-from application import app
-
-if __name__ == '__main__':
-    app.run()
